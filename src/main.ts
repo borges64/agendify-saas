8c5779e4345dd63e132777bf57e2f0df23f275b7
borges64 <pedro20002victor@gmail.com>
2025-07-24 14:28:31 -0300

    Return to NESTJS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:5173', 
+    // origin: 'http://127.0.0.1:5173',
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     credentials: true,
   })
